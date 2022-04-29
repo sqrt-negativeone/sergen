0e8052af298105d5a1a9befcb4972f1375e34157
sqrtminusone <moad_fakhri_1999@outlook.com>
2022-04-29 14:56:51 +0000

    make build folder if not exist

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 
 set opts=-FC -GR- -EHa- -nologo -Zi
 set code=%cd%
+if not exist build mkdir build
 pushd build
 cl %opts% %code%\code\main.c -Fesergen.exe
 popd
--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 code="$PWD"
 opts=-g
+if [ ! -d build ] 
+then 
+	mkdir build 
+fi 
 cd build > /dev/null
 gcc $opts $code/code/main.c -o sergen
 cd $code > /dev/null
